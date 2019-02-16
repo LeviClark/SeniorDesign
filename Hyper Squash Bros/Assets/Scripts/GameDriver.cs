66c4c81399926598980e6c53d6c22e90bdc2c1de
Samuel Engert <maynard-2006@hotmail.com>
2019-02-15 19:27:11 -0600

    Added learning materials

---
+++ Hyper Squash Bros/Assets/Scripts/GameDriver.cs
@@ -0,0 +1,14 @@
+﻿/*This script handles all logic of state transitions from the start of the game to the player winning/losing.
+ * 
+ * 
+ * 
+ * 
+ */
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GameDriver : MonoBehaviour
+{
+    
+}
---
+++ Hyper Squash Bros/Assets/Scripts/UIDriver.cs
@@ -0,0 +1,15 @@
+﻿/* Hook all UI actions through this script. A UI action should not be calling a function from anywhere but this script.
+ * 
+ * 
+ * 
+ * 
+ */
+
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class UIDriver : MonoBehaviour
+{
+    
+}
