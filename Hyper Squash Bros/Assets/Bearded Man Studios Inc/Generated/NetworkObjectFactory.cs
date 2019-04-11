544052bf31fa3659d6c351e0eb0d8d59dc0c5338
Shossain97 <shafeen.hossain97@gmail.com>
2019-04-11 16:43:33 -0500

    Playerspawning
    Y

--- Hyper Squash Bros/Assets/Bearded Man Studios Inc/Generated/NetworkObjectFactory.cs
+++ Hyper Squash Bros/Assets/Bearded Man Studios Inc/Generated/NetworkObjectFactory.cs
@@ -47,6 +47,10 @@ namespace BeardedManStudios.Forge.Networking.Generated
 						availableCallback = true;
 						obj = new TestNetworkObject(networker, id, frame);
 						break;
+					case PlayerDemoCubeNetworkObject.IDENTITY:
+						availableCallback = true;
+						obj = new PlayerDemoCubeNetworkObject(networker, id, frame);
+						break;
 				}
 
 				if (!availableCallback)
---
+++ Hyper Squash Bros/Assets/Scripts/PlayerDemoCube.cs
@@ -0,0 +1,46 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using BeardedManStudios.Forge.Networking.Generated;
+
+public class PlayerDemoCube : PlayerDemoCubeBehavior
+{
+    public float speed = 5.0f;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (!networkObject.IsOwner)
+        {
+            transform.position = networkObject.Position;
+
+            transform.rotation = networkObject.Rotation;
+
+            return;
+        }
+        Vector3 translation = new Vector3(Input.GetAxis("Horizontal"), 0, Input.GetAxis("Vertical")).normalized;
+
+        // Scale the speed to normalize for processors
+        translation *= speed * Time.deltaTime;
+
+        // Move the object by the given translation
+        transform.position += translation;
+
+        // Just a random rotation on all axis
+        transform.Rotate(new Vector3(speed, speed, speed) * 0.25f);
+
+        // Since we are the owner, tell the network the updated position
+        networkObject.Position = transform.position;
+
+        // Since we are the owner, tell the network the updated rotation
+        networkObject.Rotation = transform.rotation;
+
+    }
+
+}
