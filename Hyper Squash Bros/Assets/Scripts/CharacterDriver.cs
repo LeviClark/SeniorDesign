59a86575f51d9e11758ca3f244e26bf92c9193fa
Samuel Engert <maynard-2006@hotmail.com>
2019-04-24 09:58:26 -0500

    Added combat checking functionality
    Makes use of RPC calls to apply damage to a character after it has been check for being hit by a raycast. Left commented code in that could be used for knockbacks.

--- Hyper Squash Bros/Assets/Scripts/CharacterDriver.cs
+++ Hyper Squash Bros/Assets/Scripts/CharacterDriver.cs
@@ -2,23 +2,51 @@
 using System.Collections.Generic;
 using UnityEngine;
 using BeardedManStudios.Forge.Networking.Generated;
+using BeardedManStudios.Forge.Networking;
+using BeardedManStudios.Forge.Networking.Unity;
 
 public class CharacterDriver : CharacterDriverBehavior
 {
     public float walkSpeed = 50f;
     public float gravity = -2;
     public float jumpHeight = 2f;
-    public bool canDoubleJump = true;
+    public float forceDamageModifier = 1.0f; //Percentage base damage/force modifier
+    public float health = 100.0f;
+    public Vector3 velocity;//The actual velocity vector that the character is currently moving along
 
     private CharacterController controller;
     private Camera cam;
-    private Vector3 velocity;//The actual velocity vector that the character is currently moving along
     private float speedY;
+    private bool canDoubleJump = true;
+    private bool recoil = false;//Used to "ragdoll" punches
 
     protected override void NetworkStart()
     {
         base.NetworkStart();
         controller = GetComponent<CharacterController>();
+    }
+
+    public override void TakeDamage(RpcArgs args)
+    {
+        //RPCs aren't usually run on unity's main thread for performance, since we're using it
+        //to directly modify a GameObject we want to use the main thread
+        MainThreadManager.Run(() =>
+        {
+            //Code for knockbacks
+            //this.forceDamageModifier = forceDamageModifier + .1f;
+            //this.StartCoroutine("ResetRecoil", forceDamageModifier);
+            //this.recoil = true;
+
+            health -= 10;
+        });
+    }
+
+    private IEnumerator ResetRecoil(float waitTime) {
+        while (true) {
+            yield return new WaitForSeconds(waitTime * 2);
+            this.recoil = false;
+            yield return null;
+        }
     }
 
     // Update is called once per frame
@@ -28,7 +56,6 @@ public class CharacterDriver : CharacterDriverBehavior
         if (Input.GetKey(KeyCode.Escape)) {
             Application.Quit();
         }
-
 
         //If this isn't our character just update its values client-side
         if (!networkObject.IsOwner)
@@ -65,20 +92,21 @@ public class CharacterDriver : CharacterDriverBehavior
         //Apply gathered input to character position/rotation
         //transform.Translate(Vector3.forward * Time.deltaTime * moveForward);
         //controller.Move(velocity);
-        if (direction > 0)
+        if (direction > 0 && !recoil)
         {
-            gameObject.transform.eulerAngles = new Vector3(0, -90, 0);
-            velocity = -(transform.forward * Time.deltaTime * 5 * walkSpeed);
+            gameObject.transform.eulerAngles = new Vector3(0, 90, 0);
+            velocity = transform.forward * Time.deltaTime * 5 * walkSpeed;
         }
         else if (direction < 0)
         {
-            gameObject.transform.eulerAngles = new Vector3(0, 90, 0);
-           velocity = -(transform.forward * Time.deltaTime * 5 * walkSpeed);
+            gameObject.transform.eulerAngles = new Vector3(0, -90, 0);
+           velocity = transform.forward * Time.deltaTime * 5 * walkSpeed;
         }
         else {
             velocity = Vector3.zero;
         }
 
+        MeleeAttack();
 
         //Send the updated positions and rotations over the network
         networkObject.position = transform.position;
@@ -100,6 +128,33 @@ public class CharacterDriver : CharacterDriverBehavior
                 canDoubleJump = false;
             }
         }
+    }
+
+    void MeleeAttack() {
+        if (Input.GetMouseButtonDown(0)) {
+            Vector3 forward = transform.TransformDirection(-(Vector3.forward)) * 3;
+            Debug.DrawRay(transform.position, forward, Color.blue);//Used for visual debugging
+            RaycastHit hit;
+
+            if (Physics.Raycast(transform.position, forward, out hit)) {
+                Debug.Log("Hit " + hit.collider.gameObject.tag + "!");
+                if (hit.collider.gameObject.tag == "Player") {
+                    //Grab reference to the player's RPC call and call it on all clients
+                    hit.collider.gameObject.GetComponent<CharacterDriver>().networkObject.SendRpc(RPC_TAKE_DAMAGE, Receivers.All);
+
+                    //Used for knockback
+                    //Vector3 dir = hit.collider.gameObject.transform.position - gameObject.transform.position;
+                    //dir.Normalize();
+                    //hit.collider.gameObject.GetComponent<CharacterDriver>().velocity = dir * hit.collider.gameObject.GetComponent<CharacterDriver>().forceDamageModifier * 5;
+                }
+            }
+        }
+    }
+
+    //For use when taking hits/knockbacks
+    public void SetVelocity(Vector3 dir, float force) {
+        velocity = dir;
+        velocity = velocity * force;
     }
     
 }
