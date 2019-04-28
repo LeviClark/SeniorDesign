84c3eeecf03d617001fa21a08f927909511662aa
Shossain97 <shafeen.hossain97@gmail.com>
2019-04-28 15:22:27 -0500

    committting cause auto unity garb

--- Hyper Squash Bros/Assets/Scripts/CharacterDriver.cs
+++ Hyper Squash Bros/Assets/Scripts/CharacterDriver.cs
@@ -108,13 +108,13 @@ public class CharacterDriver : CharacterDriverBehavior
 
         if (direction > 0 && !recoil)
         {
-           // myAnimator.CrossFade("Run", 0.1f);
+            myAnimator.CrossFade("Run", 0.1f);
             gameObject.transform.eulerAngles = new Vector3(0, 90, 0);
             velocity = transform.forward * Time.deltaTime * 5 * walkSpeed;
         }
         else if (direction < 0)
         {
-            //myAnimator.CrossFade("Armature|Run", 0.1f);
+            myAnimator.CrossFade("Run", 0.1f);
             gameObject.transform.eulerAngles = new Vector3(0, -90, 0);
            velocity = transform.forward * Time.deltaTime * 5 * walkSpeed;
         }
@@ -122,26 +122,24 @@ public class CharacterDriver : CharacterDriverBehavior
             velocity = Vector3.zero;
         }
 
-        if (Input.GetMouseButtonDown(0) && Input.GetKey(KeyCode.A))
+        /*
+        if (Input.GetMouseButtonDown(0))
         {
-            myAnimator.SetBool("isAttackingLeft", true);
-        }
-        if (Input.GetMouseButtonDown(0) && Input.GetKey(KeyCode.D))
-        {
-            myAnimator.SetBool("isAttackingRight", true);
-        }
-        else if (Input.GetMouseButtonDown(0)){
-            myAnimator.SetBool("isAttackingRight", true);
+            myAnimator.SetBool("isHitting", true);
+            myAnimator.SetBool("isIdle", false);
+            myAnimator.CrossFade("Punch2", 0.1f);
+            MeleeAttack();
+               
         }
         else
         {
-            myAnimator.SetBool("isAttackingLeft", false);
-            myAnimator.SetBool("isAttackingRight", false);
-
+            myAnimator.SetBool("isHitting", false);
+            myAnimator.SetBool("isIdle", true);
         }
+        */
 
-
-        MeleeAttack();
+        
+        
 
         //Send the updated positions and rotations over the network
         networkObject.position = transform.position;
