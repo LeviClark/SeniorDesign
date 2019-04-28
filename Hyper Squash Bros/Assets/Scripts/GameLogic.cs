462e977ae66763dc368f05620f9df53148271d51
Shossain97 <shafeen.hossain97@gmail.com>
2019-04-28 15:14:23 -0500

    Spawns fully working
    Spawns and animations fully working forge sucks

--- Hyper Squash Bros/Assets/Scripts/GameLogic.cs
+++ Hyper Squash Bros/Assets/Scripts/GameLogic.cs
@@ -1,13 +1,16 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using System;
 using BeardedManStudios.Forge.Networking.Unity;
 public class GameLogic : MonoBehaviour
 {
     // Start is called before the first frame update
     void Start()
     {
-        NetworkManager.Instance.InstantiateCharacterDriver();
+        System.Random r = new System.Random();
+        int rInt = r.Next(-11, 11);
+        NetworkManager.Instance.InstantiateCharacterDriver(position:  new Vector3(rInt, 14));
     }
 
 }
